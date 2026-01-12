af79703e683bf353c2db8045973beb2abc5a2315
Sahil Ostwal <89473512+Sahilostwal@users.noreply.github.com>
2026-01-12 23:05:09 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
-# 🔒 TEMPORARY DECOMMISSION / MAINTENANCE MODE
-st.error("🚧 This application is temporarily unavailable.Please compare record count manually(•ᴗ•).")
-st.stop()
+
 import re
 import pandas as pd
 from io import BytesIO
@@ -225,5 +223,6 @@ st.markdown("""
     Developed by <a href="https://github.com/sahilostwal" target="_blank">sahilostwal</a>
 </div>
 """, unsafe_allow_html=True)
+
 
 
